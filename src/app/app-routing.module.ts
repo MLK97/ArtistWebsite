743a5d20c8630ae85d038fb0268ac31054fbf15c
MLK97 <maximilianlukaskonrad@hotmail.de>
2022-07-10 20:13:15 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { LinktreeComponent } from './linktree/linktree.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ToolsComponent } from './tools/tools.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'tools', component: ToolsComponent },
+  { path: 'linktree', component: LinktreeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/linktree/linktree.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'linktree',
+  templateUrl: './linktree.component.html',
+  styleUrls: ['./linktree.component.scss'],
+})
+export class LinktreeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
