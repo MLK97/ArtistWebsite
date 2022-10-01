680b5287bdc5e02e345db61e448656f5e8005527
MLK97 <maximilianlukaskonrad@hotmail.de>
2022-10-01 11:52:25 +0200

    amplify

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
