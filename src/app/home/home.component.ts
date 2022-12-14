b4ba6d370124576e3f83de7ed8f294db03b15211
MLK97 <maximilianlukaskonrad@hotmail.de>
2022-12-14 19:12:01 +0100

    update with locomotive scroll

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import LocomotiveScroll from 'locomotive-scroll';
+import { ResizeObserver } from '@juggle/resize-observer';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  scroll: any;
+
+  @ViewChild('scrollContent') scrollContent!: ElementRef;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]') as HTMLElement,
+      smooth: true,
+      smartphone: {
+        smooth: true,
+      },
+    });
+  }
+
+  ngAfterViewInit() {
+    const ro = new ResizeObserver((entries, observer) => {
+      entries.forEach((entry, index) => {
+        const { inlineSize: width, blockSize: height } =
+          entry.contentBoxSize[0];
+        if (this.scroll) {
+          this.scroll.update();
+        }
+      });
+    });
+
+    ro.observe(this.scrollContent.nativeElement);
+  }
 }
